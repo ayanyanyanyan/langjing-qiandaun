273465a0b99cb1aa9348242e069cab6e672ddcff
ayanyanyanyan <3280838928@qq.com>
2023-11-23 19:03:23 +0800

    Initial commit

---
+++ src/views/student-report/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <el-tiptap v-model="content" :extensions="extensions" />
+  </div>
+</template>
+
+<script>
+import {
+  // 需要的 extensions
+  Doc,
+  Text,
+  Paragraph,
+  Heading,
+  Bold,
+  Underline,
+  Italic,
+  Strike,
+  ListItem,
+  BulletList,
+  OrderedList,
+} from 'element-tiptap';
+
+export default {
+  data () {
+    // 编辑器的 extensions
+    // 它们将会按照你声明的顺序被添加到菜单栏和气泡菜单中
+    return {
+      extensions: [
+        new Doc(),
+        new Text(),
+        new Paragraph(),
+        new Heading({ level: 5 }),
+        new Bold({ bubble: true }), // 在气泡菜单中渲染菜单按钮
+        new Underline({ bubble: true, menubar: false }), // 在气泡菜单而不在菜单栏中渲染菜单按钮
+        new Italic(),
+        new Strike(),
+        new ListItem(),
+        new BulletList(),
+        new OrderedList(),
+      ],
+      // 编辑器的内容
+      content: `
+        <h1>Heading</h1>
+        <p>This Editor is awesome!</p>
+      `,
+    };
+  },
+}
+</script>
